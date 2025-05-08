f50925c69729a029c3d2ea08015d908043c945e8
kavinkundadadam <kavinkundadam@gmail.com>
2025-05-08 15:50:51 +0530

    Errors are resolved

--- jsclasses.js
+++ jsclasses.js
@@ -5,18 +5,18 @@ class Person {
     }
 }
 
-const me = new Person();
-const you = new Person();
-console.log(me.talk());
-console.log(you.talk());
-console.log(you);
+const someone = new Person();
+const otherone = new Person();
+console.log(someone.talk());
+console.log(otherone.talk());
+console.log(otherone);
 
 console.log(Object.prototype.constructor === Object);
 
 Person.prototype.greet = function() {
     return "Hi, Welcome to web Programming with pk";
 }
-console.log(me.greet());
+console.log(someone.greet());
 
 // Getter & Setter
 class Person1 {
@@ -34,112 +34,11 @@ class Person1 {
         this.name = newName;
     }
 }
-let person = new Person1("Kavin Manudheeran");
-console.log(person); // show's that object fully
+let onePerson = new Person1("Kavin Manudheeran");
+console.log(onePerson); // show's that object fully
 // console.log(person.getName()); // Kavin Manudheeran
 person.setName("Magil Mithra");
-console.log(person.getName()); //Magil Mithra
-
-// Get & Set by keyword
-class Person2 {
-    constructor(name) {
-        this._name = name; // Here we made mistake while assigning
-    }
-    get name() {           // -> By keyword
-        return this._name;
-    }
-    set name(newName) {
-        newName = newName.trim();
-        if (newName === '') {
-            return"Name can't ne empty";
-        }
-        this._name = newName;
-    }
-}
-let firstPerson = new Person2("Manu Dheeran");
-console.log(firstPerson.name);
-
-//class expression
-
-let Person3 = class {
-    constructor(name) {
-        this.name = name;
-    }
-    getName(){
-        return this.name;
-    }
-}
- let secondPerson = new Person3("Kavin Palanisamy");
-
- //Singleton
-
- let web = new class {
-    constructor(name){
-        this.name = name;
-    }
-    load() {
-        console.log(`loading the web named ${this.name}`);
-    }
- }("CocoFarmer's");
-web.load();
-
-//Computated property
-
-const nextName = 'fullName';
-class Person4 {
-    constructor(firstName, lastName) {
-        this.firstName = firstName;
-        this.lastName =  lastName;
-    }
-    get [nextName]() {
-        return `${this.firstName} ${this.lastName}`;
-    }
-}
-let nextPerson = new Person4("Kavin", "Kundadam");
-//console.log(nextPerson.fullName()); 
-/*Uncaught TypeError: nextPerson.fullName is not a "function"*/
- 
-// JavaScript Class 
-class Person {
-    talk(){
-        return "Talking";
-    }
-}
-
-const me= new Person();
-const you = new Person();
-console.log(me.talk());
-console.log(you.talk());
-console.log(you);
-
-console.log(Object.prototype.constructor === Object);
-
-Person.prototype.greet = function() {
-    return "Hi, Welcome to web Programming with pk";
-}
-console.log(me.greet());
-
-// Getter & Setter
-class Person1 {
-    constructor(name) {
-        this.setName(name);
-    }
-    getName() {              // -> By userdefined
-        return this.name;
-    }
-    setName(newName) {
-        newName = newName.trim();
-        if (newName === '') {
-            throw "Name can't be empty";
-        }
-        this.name = newName;
-    }
-}
-let person = new Person1("Kavin Manudheeran");
-console.log(person); // show's that object fully
-// console.log(person.getName()); // Kavin Manudheeran
-person.setName("Magil Mithra");
-console.log(person.getName()); //Magil Mithra
+console.log(onePerson.getName()); //Magil Mithra
 
 // Get & Set by keyword
 class Person2 {
