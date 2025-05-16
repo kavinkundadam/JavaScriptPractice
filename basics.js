6dcc65bbe190be3faed0db3ffcd9a4ab3607479c
kavinkundadam <kavinkundadam@gmail.com>
2025-05-16 09:46:19 +0530

    Made some changes in basics file

--- basics.js
+++ basics.js
@@ -1,8 +1,13 @@
+// "use strict";
+// Declaration for show cart quantity events
+let cartQuantity = 0;
+
 // Basics operations
+/*
 let a = 25.56 + 7.2 * 9.26 / 3;
 console.log("a = " + a);
 console.log("a = " + Math.round(a));
-
+*/ /*
 // string
 let sentence = "Hello"
 sentence += ' ' + "Programming.";
@@ -19,6 +24,14 @@ console.log(alsoAcceptable);
 let usingBacktick = `Items (${1 + 1}) : $${52+6}/-`; 
 console.log(usingBacktick);
 
+console.log(sentence.charCodeAt(0)); //72 -> Unicode value for Uppercase H.
+
+let copyString = "It will have access \
+to multiline and that backslash should \
+not visible in output.";
+console.log(copyString);
+*/
+/*
 //variables 
 let calculation = 2 + 3;
 console.log(calculation + 2);
@@ -50,14 +63,45 @@ console.log(product.productName);
 product.productName = 'Mobile Phone';
 console.log(product);
 
-console.log(JSON.stringify( product)); // DOes not return function!
+console.log(JSON.stringify( product)); // DOes not return function! */
 
 // Arrays
 
 let myArray = [1, 'Hello', {name : "kavin"}, 98 , [1,2]];
 console.log(myArray);
-myArray.splice(3,98);
-console.log(myArray);
 
-// Declaration for show cart quantity events
-let cartQuantity = 0;
+let bikes = ['MT-15', 'R-15', 'RX-135', 'RX-100'];
+bikes.unshift('TVS-50');
+console.log(bikes);
+console.log("Length : " + bikes.length);
+
+bikes[bikes.length] = 'Duke-390';
+console.log(bikes);
+
+// Using delete() leaves undefined holes in the array.
+// Use pop() or shift() instead. 
+
+// delete bikes[4]; //[ 'TVS-50', 'MT-15', 'R-15', 'RX-135', <1 empty item>, 'Duke-390' ]
+// console.log(bikes);
+
+bikes.pop(); 
+console.log(bikes);
+
+bikes.splice(2,3,'Unicorn', 'CT-100'); //[ 'TVS-50', 'MT-15', 'Unicorn', 'CT-100' ]
+console.log(bikes);
+
+bikes.splice(1,1); // Splice(Position, No.of.Elements removed)
+console.log(bikes);
+
+
+let myName = "Kavin";
+console.log(myName);
+
+var txt = "";
+var numbers = [45, 4, 9, 16, 25];
+numbers.forEach(myFunction);
+console.log(txt);
+
+function myFunction(value) {
+  txt += value + '\n'; 
+}
